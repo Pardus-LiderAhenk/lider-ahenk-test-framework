c9cc513535976a38954fa5730b52f92b56f2ac66
İsmail Başaran <basaran.ismaill@gmail.com>
2016-05-12 17:30:58 +0300

    embedded mariadb

---
+++ lider-embedded-db/src/main/java/tr/org/liderahenk/MariaDBTest.java
@@ -0,0 +1,22 @@
+package tr.org.liderahenk;
+
+import ch.vorburger.exec.ManagedProcessException;
+import ch.vorburger.mariadb4j.DB;
+import ch.vorburger.mariadb4j.DBConfigurationBuilder;
+
+public class MariaDBTest {
+	
+	public static void main(String...strings){
+		DBConfigurationBuilder configBuilder = DBConfigurationBuilder.newBuilder();
+		configBuilder.setPort(3306); // OR, default: setPort(0); => autom. detect free port
+		configBuilder.setDataDir("/tmp/db"); // just an example
+		try {
+			DB db = DB.newEmbeddedDB(configBuilder.build());
+			db.start();
+			db.createDB("liderdb");
+		} catch (ManagedProcessException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
